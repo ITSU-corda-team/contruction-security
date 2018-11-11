d646ffa6080b94e8e9587e138a5608e6f832320a
YashwineeKarkera <yashwineegk@gmail.com>
2018-11-11 22:31:51 +0000

    Changes in 1. StatesAndContacts to include Project State and Contaract Handling 2. Build.gradle to update the nodes - we will have only 3 nodes - SecTrustee, Bank and Offtaker 3. Flows

---
+++ cordapp-itsu-kotlin/cordapp/src/main/kotlin/net/cordaclub/itsu/Projects.kt
@@ -0,0 +1,37 @@
+package net.cordaclub.itsu
+/*
+import net.corda.core.identity.CordaX500Name
+import net.corda.core.identity.Party
+
+object Projects {
+    // TODO YGK - Add meaningful project names
+    val allProjects = listOf(
+            Project("HK_Macau_Bridge_Success", 20000000, 20000000,
+                    0, 20000000),
+            Project("HK Macau Bridge-Fail", 10000000, 10000000, 0, 10000000)
+    )
+}
+
+
+object Banks {
+    val allBanks = listOf(
+            CordaX500Name("BankOfChina", "Beijing", "CN"),
+            CordaX500Name("HSBC", "London", "UK")
+    )
+}
+
+object Offtakers {
+    val allOfftakers = listOf(
+            CordaX500Name("ChinaCentralGovt", "Beijing", "CH"),
+            CordaX500Name("HongkongGovt", "Hongkong", "HK")
+    )
+}
+
+
+object SecurityTrustees {
+    val allSecurityTrustees = listOf(
+            CordaX500Name("Sec Trustee 1", "Beijing", "CH"),
+            CordaX500Name("Sec Trustee 2", "Hongkong", "HK")
+    )
+}
+        */
